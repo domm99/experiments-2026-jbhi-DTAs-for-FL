cc69aeb8ff137004db430387c9cc68bb38dbd964
Davide Domini <davide.domini@unibo.it>
2026-06-23 11:05:01 +0200

    feat: implement FL server with FedAvg

---
+++ src/FLServer.py
@@ -0,0 +1,36 @@
+from LearningConfig import LearningConfig
+
+class FLServer:
+
+    def __init__(self, config: LearningConfig,):
+        self._config = config
+        self.clients_models = []
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._model = GlucoseClassifierLSTM(
+            hidden_size=self._config.hidden_size,
+            num_layers=self._config.layers,
+            dropout=self._config.dropout,
+        ).to(self._config.device)
+
+    def aggregate(self):
+        """
+        Aggregates N models following the FedAvg algorithm.
+        :return: Nothing
+        """
+        models = self.clients_models
+        w_avg = copy.deepcopy(models[0])
+
+        for key in w_avg.keys():
+            w_avg[key] = torch.mul(w_avg[key], 0.0)
+        for key in w_avg.keys():
+            for i in range(0, len(models)):
+                w_avg[key] += models[i][key]
+            w_avg[key] = torch.div(w_avg[key], len(models))
+        self._model.load_state_dict(w_avg)
+
+    def receive_client_update(self, clients_models):
+        self.clients_models = clients_models
+
+    @property
+    def model(self):
+        return self._model
